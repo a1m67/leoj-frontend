8b03522045cff8e3d1ae924443c845f651a16979
a1m67 <243363113@qq.com>
2024-05-19 03:47:58 +0800

    global menu

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="globalHeader">
+    <a-menu
+      mode="horizontal"
+      :selected-keys="selectKeys"
+      :default-selected-keys="['1']"
+      @menu-item-click="doMenuClick"
+    >
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/oj-logo.svg" />
+          <div class="title">樂 OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">
+        {{ item.name }}
+      </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { routes } from "../router/routes";
+import { useRoute, useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+
+// 路由跳转时更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectKeys.value = [to.path];
+});
+
+// 默认主页
+const selectKeys = ref(["/"]);
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">by a1m67</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #aaa, #fff);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
